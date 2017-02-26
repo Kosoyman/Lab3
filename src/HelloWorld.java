9855c84bccd274f6631073d7137f856edf82e446
Maxim <mk223hm@student.lnu.se>
2017-02-26 13:25:48 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author Maxim Kravchenko
+ * @date 26-Feb-17.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
