7e718a5c251542fdfe94e2fc134af815261dda0b
Maxim <mk223hm@student.lnu.se>
2017-02-28 15:42:55 +0100

    It's now possible to send files larger than 512 bytes (the client app will throw an error at the end of the transmission, but the received file will be ol  Receiving acknowledgement is now done in a separate method called receive_ACK()

--- src/TFTPServer.java
+++ src/TFTPServer.java
@@ -1,5 +1,3 @@
-import javax.xml.crypto.Data;
-import java.io.File;
 import java.io.IOException;
 import java.net.*;
 import java.nio.ByteBuffer;
@@ -8,180 +6,180 @@ import java.nio.file.Paths;
 
 public class TFTPServer
 {
-	private static final int TFTPPORT = 4970;
-	private static final int BUFSIZE = 516;
-	private static final String READDIR = "TFTP\\read\\";
-	private static final String WRITEDIR = "TFTP\\write\\"; //custom address at your PC
+    private static final int TFTPPORT = 4970;
+    private static final int BUFSIZE = 516;
+    private static final String READDIR = "TFTP\\read\\";
+    private static final String WRITEDIR = "TFTP\\write\\"; //custom address at your PC
 
-	// OP codes
-	private static final int OP_RRQ = 1;
-	private static final int OP_WRQ = 2;
-	private static final int OP_DAT = 3;
-	private static final int OP_ACK = 4;
-	private static final int OP_ERR = 5;
+    // OP codes
+    private static final int OP_RRQ = 1;
+    private static final int OP_WRQ = 2;
+    private static final int OP_DAT = 3;
+    private static final int OP_ACK = 4;
+    private static final int OP_ERR = 5;
 
-	public static void main(String[] args) {
-		if (args.length > 0)
-		{
-			System.err.printf("usage: java %s\n", TFTPServer.class.getCanonicalName());
-			System.exit(1);
-		}
-		//Starting the server
-		try
-		{
-			TFTPServer server= new TFTPServer();
-			server.start();
-		}
-		catch (SocketException e)
-		{e.printStackTrace();}
-	}
+    public static void main(String[] args) {
+        if (args.length > 0)
+        {
+            System.err.printf("usage: java %s\n", TFTPServer.class.getCanonicalName());
+            System.exit(1);
+        }
+        //Starting the server
+        try
+        {
+            TFTPServer server= new TFTPServer();
+            server.start();
+        }
+        catch (SocketException e)
+        {e.printStackTrace();}
+    }
 
-	private void start() throws SocketException
-	{
-		byte[] buf= new byte[BUFSIZE];
+    private void start() throws SocketException
+    {
+        byte[] buf= new byte[BUFSIZE];
 
-		// Create socket
-		DatagramSocket socket= new DatagramSocket(null);
+        // Create socket
+        DatagramSocket socket= new DatagramSocket(null);
 
-		// Create local bind point 
-		SocketAddress localBindPoint= new InetSocketAddress(TFTPPORT);
-		socket.bind(localBindPoint);
+        // Create local bind point
+        SocketAddress localBindPoint= new InetSocketAddress(TFTPPORT);
+        socket.bind(localBindPoint);
 
-		System.out.printf("Listening at port %d for new requests\n", TFTPPORT);
+        System.out.printf("Listening at port %d for new requests\n", TFTPPORT);
 
-		// Loop to handle client requests 
-		while (true)
-		{
+        // Loop to handle client requests
+        while (true)
+        {
 
-			final InetSocketAddress clientAddress = receiveFrom(socket, buf);
+            final InetSocketAddress clientAddress = receiveFrom(socket, buf);
 
-			// If clientAddress is null, an error occurred in receiveFrom()
-			if (clientAddress == null)
-				continue;
+            // If clientAddress is null, an error occurred in receiveFrom()
+            if (clientAddress == null)
+                continue;
 
-			final StringBuffer requestedFile= new StringBuffer();
-			final int reqtype = ParseRQ(buf, requestedFile);
+            final StringBuffer requestedFile= new StringBuffer();
+            final int reqtype = ParseRQ(buf, requestedFile);
 
-			new Thread()
-			{
-				public void run()
-				{
-					try
-					{
-						DatagramSocket sendSocket= new DatagramSocket(0);
+            new Thread()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        DatagramSocket sendSocket= new DatagramSocket(0);
 
-						// Connect to client
-						sendSocket.connect(clientAddress);
+                        // Connect to client
+                        sendSocket.connect(clientAddress);
 /*
 						System.out.printf("%s request for %s from %s using port %d\n",
 								(reqtype == OP_RRQ)?"Read":"Write",
 								clientAddress.getHostName(), clientAddress.getPort());
 */
-						// Read request
-						if (reqtype == OP_RRQ)
-						{
-							requestedFile.insert(0, READDIR);
-							HandleRQ(sendSocket, requestedFile.toString(), OP_RRQ);
-						}
-						// Write request
-						else
-						{
-							requestedFile.insert(0, WRITEDIR);
-							HandleRQ(sendSocket,requestedFile.toString(),OP_WRQ);
-						}
-						sendSocket.close();
-					}
-					catch (SocketException e)
-					{e.printStackTrace();}
-				}
-			}.start();
-		}
-	}
+                        // Read request
+                        if (reqtype == OP_RRQ)
+                        {
+                            requestedFile.insert(0, READDIR);
+                            HandleRQ(sendSocket, requestedFile.toString(), OP_RRQ);
+                        }
+                        // Write request
+                        else
+                        {
+                            requestedFile.insert(0, WRITEDIR);
+                            HandleRQ(sendSocket,requestedFile.toString(),OP_WRQ);
+                        }
+                        sendSocket.close();
+                    }
+                    catch (SocketException e)
+                    {e.printStackTrace();}
+                }
+            }.start();
+        }
+    }
 
-	/**
-	 * Reads the first block of data, i.e., the request for an action (read or write).
-	 * @param socket (socket to read from)
-	 * @param buf (where to store the read data)
-	 * @return socketAddress (the socket address of the client)
-	 */
-	private InetSocketAddress receiveFrom(DatagramSocket socket, byte[] buf)
-	{
-		// Create datagram packet
-		DatagramPacket dp = new DatagramPacket(buf, buf.length);
+    /**
+     * Reads the first block of data, i.e., the request for an action (read or write).
+     * @param socket (socket to read from)
+     * @param buf (where to store the read data)
+     * @return socketAddress (the socket address of the client)
+     */
+    private InetSocketAddress receiveFrom(DatagramSocket socket, byte[] buf)
+    {
+        // Create datagram packet
+        DatagramPacket dp = new DatagramPacket(buf, buf.length);
 
-		// Receive packet
-		try {
-			socket.receive(dp);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// Get client address and port from the packet
+        // Receive packet
+        try {
+            socket.receive(dp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Get client address and port from the packet
 
-		int port = dp.getPort();
+        int port = dp.getPort();
 
-		return new InetSocketAddress(dp.getAddress(), port);
-	}
+        return new InetSocketAddress(dp.getAddress(), port);
+    }
 
-	/**
-	 * Parses the request in buf to retrieve the type of request and requestedFile
-	 *
-	 * @param buf (received request)
-	 * @param requestedFile (name of file to read/write)
-	 * @return opcode (request type: RRQ or WRQ)
-	 */
-	private int ParseRQ(byte[] buf, StringBuffer requestedFile)
-	{
-		// See "TFTP Formats" in TFTP specification for the RRQ/WRQ request contents
+    /**
+     * Parses the request in buf to retrieve the type of request and requestedFile
+     *
+     * @param buf (received request)
+     * @param requestedFile (name of file to read/write)
+     * @return opcode (request type: RRQ or WRQ)
+     */
+    private int ParseRQ(byte[] buf, StringBuffer requestedFile)
+    {
+        // See "TFTP Formats" in TFTP specification for the RRQ/WRQ request contents
 
-		ByteBuffer wrap= ByteBuffer.wrap(buf);
-		short opcode = wrap.getShort();
+        ByteBuffer wrap= ByteBuffer.wrap(buf);
+        short opcode = wrap.getShort();
 
-		// We can now parse the request message for opcode and requested file as:
+        // We can now parse the request message for opcode and requested file as:
 
-		int readBytes = 2;// where readBytes is the number of bytes read into the byte array buf.
+        int readBytes = 2;// where readBytes is the number of bytes read into the byte array buf.
 
-		//filename is followed by 1 byte of 0s
-		while (buf[readBytes] != 0)
-			readBytes ++;
+        //filename is followed by 1 byte of 0s
+        while (buf[readBytes] != 0)
+            readBytes ++;
 
-		String fileName = new String(buf, 2, readBytes-2);  //converts readBytes to the length of the filename
+        String fileName = new String(buf, 2, readBytes-2);  //converts readBytes to the length of the filename
 
-		requestedFile.append(fileName); //Store the filename in the StringBuffer
+        requestedFile.append(fileName); //Store the filename in the StringBuffer
 
-		//parse transfer mode, we are supposed to do that but I am not sure what to use it for atm
+        //parse transfer mode, we are supposed to do that but I am not sure what to use it for atm
 
-		int offset = readBytes; //save the offset for mode
-		readBytes ++; //"step over" 0 that signified the end of the filename
+        int offset = readBytes; //save the offset for mode
+        readBytes ++; //"step over" 0 that signified the end of the filename
 
-		//mode is followed by 1 byte of 0s
-		while (buf[readBytes] != 0)
-			readBytes ++;
+        //mode is followed by 1 byte of 0s
+        while (buf[readBytes] != 0)
+            readBytes ++;
 
-		//readBytes - offset give length of the mode; saving the mode in lower case for convenience
-		String mode = new String(buf, offset, readBytes - offset).toLowerCase();
-		System.out.println("OPCODE: " + opcode);
-		System.out.println("FILENAME: " + fileName);
-		System.out.println("MODE: " + mode);
-		return opcode;
-	}
+        //readBytes - offset give length of the mode; saving the mode in lower case for convenience
+        String mode = new String(buf, offset, readBytes - offset).toLowerCase();
+        System.out.println("OPCODE: " + opcode);
+        System.out.println("FILENAME: " + fileName);
+        System.out.println("MODE: " + mode);
+        return opcode;
+    }
 
-	/**
-	 * Handles RRQ and WRQ requests 
-	 *
-	 * @param sendSocket (socket used to send/receive packets)
-	 * @param requestedFile (name of file to read/write)
-	 * @param opcode (RRQ or WRQ)
-	 */
+    /**
+     * Handles RRQ and WRQ requests
+     *
+     * @param sendSocket (socket used to send/receive packets)
+     * @param requestedFile (name of file to read/write)
+     * @param opcode (RRQ or WRQ)
+     */
 
-	private void HandleRQ(DatagramSocket sendSocket, String requestedFile, int opcode)
-	{
-		if(opcode == OP_RRQ)
-		{
-			// See "TFTP Formats" in TFTP specification for the DATA and ACK packet contents
-			boolean result = send_DATA_receive_ACK(sendSocket, requestedFile);
-			System.out.println("SENT SUCCESSFULLY: " + result);
-		}
-
+    private void HandleRQ(DatagramSocket sendSocket, String requestedFile, int opcode)
+    {
+        if(opcode == OP_RRQ)
+        {
+            // See "TFTP Formats" in TFTP specification for the DATA and ACK packet contents
+            boolean result = send_DATA_receive_ACK(sendSocket, requestedFile);
+            System.out.println("SENT SUCCESSFULLY: " + result);
+        }
+/*
 		else if (opcode == OP_WRQ)
 		{
 			boolean result = receive_DATA_send_ACK(params);
@@ -193,63 +191,105 @@ public class TFTPServer
 			send_ERR(params);
 			return;
 		}
+*/
+    }
 
-	}
+    /**
+     To be implemented
+     */
+    private boolean send_DATA_receive_ACK(DatagramSocket socket, String requestedFile)
+    {
+        try {
+            //form the packet to send
+            int  filePointer = 0,
+                    packetPointer;
+            InetAddress ip = socket.getInetAddress();
+            int port = socket.getPort();
+            byte blockNumber = 0;
+            byte[] file = Files.readAllBytes(Paths.get(requestedFile).normalize()),
+                    packet;
+            DatagramPacket sendPacket;
 
-	/**
-	 To be implemented
-	 */
-	//TODO: right now works for files of size 508 or less bytes
-	private boolean send_DATA_receive_ACK(DatagramSocket socket, String requestedFile)
-	{
-		try {
-			//form the packet to send
+            while (filePointer < file.length -1) {
+                packet = new byte[516];
 
-			byte[] packet = new byte[512]; //right now has max length, we might want to change that
-			//set opcode
-			packet[0] = 0;
-			packet[1] = 3;
+                //set opcode
+                packet[0] = 0;
+                packet[1] = 3;
 
-			//set block number
-			packet[2] = 0;
-			packet[3] = 1;
+                //set block number
+                blockNumber++;
 
-			byte[] file = Files.readAllBytes(Paths.get(requestedFile).normalize()); //read the file into byte array
+                if (blockNumber < 256) {
+                    packet[2] = 0;
+                    packet[3] = blockNumber;
+                    //if the block number gets too big for byte #3 to hold, increment byte #2 and reset byte #3
+                } else {
+                    packet[2] ++;
+                    blockNumber = 0;
+                    packet[3] = blockNumber;
+                }
 
-			//set data
-			System.arraycopy(file, 0, packet, 4, file.length);
+                //copy as much as possible from file into the packet
+                for (packetPointer = 4; packetPointer < packet.length; packetPointer++) {
+                    if(filePointer < file.length){
+                        packet[packetPointer] = file[filePointer];
+                        filePointer++;
+                    }
+                    else
+                        break;
+                }
 
-			DatagramPacket sendPacket = new DatagramPacket(packet, packet.length, socket.getInetAddress(), socket.getPort());
+                sendPacket = new DatagramPacket(packet, packet.length, ip, port);
+                socket.send(sendPacket);//send
 
-			socket.send(sendPacket);//send
+                //receiving acknowledgement for packet
+                short bn  = receive_ACK(socket);
 
-			//receving acknowledgement for packet
-			byte[] ACKbuf = new byte[4]; //ACK packet is 4 bytes long (RFC1350)
-			DatagramPacket receivePacket = new DatagramPacket(ACKbuf, ACKbuf.length);
-			socket.receive(receivePacket);
+                System.out.println("BLOCK NUMBER: " + bn);
 
-			byte[] ACK = receivePacket.getData();
+                //if the block number is not ok
+                if (bn != blockNumber) {
+
+                    while (bn != blockNumber) {
+                        socket.send(sendPacket); //retransmit
+                        bn = receive_ACK(socket); //receive ACK
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private short receive_ACK(DatagramSocket socket){
+        byte[] ACKbuf = new byte[4]; //ACK packet is 4 bytes long (RFC1350)
+        DatagramPacket receivePacket = new DatagramPacket(ACKbuf, ACKbuf.length);
+        try {
+            socket.receive(receivePacket);
+
+            byte[] ACK = receivePacket.getData();
             ByteBuffer wrap= ByteBuffer.wrap(ACK);
 
             short opcode = wrap.getShort(); //parse opcode
-            short blockNumber = wrap.getShort(); //parse block number
-            System.out.println("ACK (?) OPCODE: " + opcode);
-            System.out.println("BLOCK NUMBER: " + blockNumber);
+            if(opcode == OP_ACK) {
+                return wrap.getShort(); //parse block number
+            }
 
-            return (opcode == 4 && blockNumber == 1); //check that the ACK is correct and return the result
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+/*
 	private boolean receive_DATA_send_ACK(params)
 	{return true;}
 
 	private void send_ERR(params)
 	{}
-
+*/
 }
 
 
